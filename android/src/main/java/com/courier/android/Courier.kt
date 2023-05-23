c1596b377e30952016e0974c61f844bffa545c23
mikemilla <mike@mikemiller.design>
2023-05-23 12:53:14 -0700

    Release

--- android/src/main/java/com/courier/android/Courier.kt
+++ android/src/main/java/com/courier/android/Courier.kt
@@ -54,7 +54,7 @@ class Courier private constructor(internal val context: Context) {
     companion object {
 
         var USER_AGENT = CourierAgent.NATIVE_ANDROID
-        internal const val VERSION = "2.0.01"
+        internal const val VERSION = "2.0.1"
         internal const val TAG = "Courier SDK"
         internal const val COURIER_PENDING_NOTIFICATION_KEY = "courier_pending_notification_key"
         internal val eventBus by lazy { NotificationEventBus() }
