381d4c0f9c2483d20fb91ce6f46f25150004aa1d
mikemilla <mike@mikemiller.design>
2023-11-16 10:25:32 -0800

    Release

--- app/src/main/java/com/courier/example/fragments/PushFragment.kt
+++ app/src/main/java/com/courier/example/fragments/PushFragment.kt
@@ -9,11 +9,17 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.courier.android.Courier
+import com.courier.android.models.CourierPushEvent
 import com.courier.android.models.CourierPushProvider
 import com.courier.android.modules.fcmToken
 import com.courier.android.modules.getToken
+import com.courier.android.modules.setToken
+import com.courier.android.utils.trackNotification
 import com.courier.example.R
+import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.launch
 
 
 class PushFragment : Fragment(R.layout.fragment_push) {
