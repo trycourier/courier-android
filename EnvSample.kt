94ddfccffbd692ed3014fc89dfece3bf29d3a70e
mikemilla <mike@mikemiller.design>
2024-07-03 11:04:51 -0400

    Env sample updates

--- EnvSample.kt
+++ EnvSample.kt
@@ -1,14 +1,16 @@
 package PACKAGE_NAME
 
 class Env {
-
     companion object {
-        const val COURIER_AUTH_KEY = "XXXXX"
-        const val COURIER_USER_ID = "XXXXX"
-        const val FIREBASE_API_KEY = "XXXXX"
-        const val FIREBASE_APP_ID = "XXXXX"
-        const val FIREBASE_PROJECT_ID = "XXXXX"
-        const val FIREBASE_GCM_SENDER_ID = "XXXXX"
+        const val COURIER_AUTH_KEY = "XXXXXX"
+        const val COURIER_CLIENT_KEY = "XXXXXX"
+        const val COURIER_BRAND_ID = "XXXXXX"
+        const val COURIER_PREFERENCE_ID = "XXXXXX"
+        const val COURIER_USER_ID = "XXXXXX"
+        const val FIREBASE_API_KEY = "XXXXXX"
+        const val FIREBASE_APP_ID = "XXXXXX"
+        const val FIREBASE_PROJECT_ID = "XXXXXX"
+        const val FIREBASE_GCM_SENDER_ID = "XXXXXX"
     }
 
 }
