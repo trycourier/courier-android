6bf6461d9aee61ee0cb5b44d3b53d2e1ad009a82
mikemilla <mike@mikemiller.design>
2024-07-18 14:00:00 -0400

    🚀 4.0.0

--- android/src/main/java/com/courier/android/Courier.kt
+++ android/src/main/java/com/courier/android/Courier.kt
@@ -52,7 +52,7 @@ class Courier private constructor(internal val context: Context) : Application.A
 
         // Core
         var USER_AGENT = CourierAgent.NATIVE_ANDROID
-        internal const val VERSION = "3.5.13"
+        internal const val VERSION = "4.0.0"
         internal const val TAG = "Courier SDK"
 
         // Push
