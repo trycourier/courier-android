637af13a65546e69f6d9ca1ce6991205d3d1424a
mikemilla <mike@mikemiller.design>
2024-10-17 15:33:49 -0700

    🚀 4.5.1

--- android/src/main/java/com/courier/android/Courier.kt
+++ android/src/main/java/com/courier/android/Courier.kt
@@ -71,7 +71,7 @@ class Courier private constructor(val context: Context) : Application.ActivityLi
     companion object {
 
         // Core
-        private const val VERSION = "4.5.0"
+        private const val VERSION = "4.5.1"
         var agent: CourierAgent = CourierAgent.NativeAndroid(VERSION)
 
         // Push
