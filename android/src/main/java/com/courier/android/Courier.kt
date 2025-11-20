ce1ced9754ef76c66a8619200131a92cab867581
Mike Miller <mike@mikemiller.design>
2025-11-20 13:23:21 -0700

    Bump

--- android/src/main/java/com/courier/android/Courier.kt
+++ android/src/main/java/com/courier/android/Courier.kt
@@ -50,7 +50,7 @@ class Courier private constructor(val context: Context) : Application.ActivityLi
     companion object {
 
         // Core
-        private const val VERSION = "5.2.12"
+        private const val VERSION = "5.2.13"
         var agent: CourierAgent = CourierAgent.NativeAndroid(VERSION)
 
         // Inbox
